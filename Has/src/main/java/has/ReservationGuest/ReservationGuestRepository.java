3561334aca5707d87da483051e0f9c74c1b3094a
Kaloyan Getsov <kaloiangecov@abv.bg>
2017-01-31 18:22:49 +0200

    added repo,service,controller for reservation guest, some extra data to the sql file

---
+++ Has/src/main/java/has/ReservationGuest/ReservationGuestRepository.java
@@ -0,0 +1,11 @@
+package has.ReservationGuest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by kaloi on 1/31/2017.
+ */
+@Repository
+public interface ReservationGuestRepository extends JpaRepository<ReservationGuest, Long> {
+}
