891b973fe1fb4884c194c227c4c07d5fc700953d
Kaloyan Getsov <kaloiangecov@abv.bg>
2017-03-18 16:53:31 +0200

    some stuff

--- Has/src/main/java/has/MealType/Meal/MealCategoryService.java
+++ Has/src/main/java/has/MealType/Meal/MealCategoryService.java
@@ -25,7 +25,6 @@ public class MealCategoryService {
 
     public List<MealCategory> getAllMealCategories() {
         List<MealCategory> categories = repo.findAll();
-
         return categories;
     }
 
