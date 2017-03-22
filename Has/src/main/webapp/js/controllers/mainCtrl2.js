aff72543bcc3664e50573ecefde4ccf7ce0df20c
Stefan <hardmaster.92@hotmail.com>
2017-03-22 11:41:02 +0200

    Calendar: only room id is passed on reservaton creation. Booking removed unnesessary user role search.

--- Has/src/main/webapp/js/controllers/mainCtrl2.js
+++ Has/src/main/webapp/js/controllers/mainCtrl2.js
@@ -83,23 +83,6 @@ app2.controller("mainCtrl2", function ($rootScope, $scope, $state, $http, $timeo
         }
     };
 
-    $scope.getRole = function (roleID, callback) {
-        $http({
-            method: "GET",
-            url: ("role/" + roleID),
-            responseType: "json",
-            headers: {
-                "Authorization": $scope.authentication
-            }
-        }).then(
-            function (response) { //success
-                return response.data;
-            },
-            function (response) { //error
-                $scope.displayMessage(response.data);
-            }).then(callback);
-    };
-
     $scope.getGuestUserByEmail = function (email) {
         $http({
             method: "POST",
@@ -294,15 +277,15 @@ app2.controller("mainCtrl2", function ($rootScope, $scope, $state, $http, $timeo
         $scope.results = [];
     };
 
-    $scope.addReservationsToGroup = function (mainReservations) {
-        delete mainReservations.reservationCode;
-        delete mainReservations.id;
+    $scope.addReservationsToGroup = function (mainReservation) {
+        delete mainReservation.reservationCode;
+        delete mainReservation.id;
 
         for (var i = 1; i < $scope.selectedRooms.length; i++) {
-            $scope.saveData("reservation?group=true&groupId=" + (mainReservations.groupId),
-                angular.extend({}, mainReservations, {room: $scope.selectedRooms[i]}),
+            $scope.saveData("reservation?group=true&groupId=" + (mainReservation.groupId),
+                angular.extend({}, mainReservation, {room: $scope.selectedRooms[i]}),
                 function (newGroup) {
-                    console.log('additional group ' + (i + 1), mainReservations);
+                    console.log('additional group ' + (i + 1), mainReservation);
                 });
         }
     };
@@ -324,32 +307,30 @@ app2.controller("mainCtrl2", function ($rootScope, $scope, $state, $http, $timeo
             $scope.reservationGuest.reservation = newReservation;
 
             if (!$scope.guest.user.username && $scope.guest.user.email) { // create new guest
-                $scope.getRole(5, function (role) {
-                    $scope.guest.user.username = $scope.guest.user.email;
-                    $scope.guest.user.password = base64encode($scope.guest.user.email);
-                    $scope.guest.user.userRole = role;
-                    $scope.guest.user.regDate = (new Date()).toISOString().substr(0, 10);
+                $scope.guest.user.username = $scope.guest.user.email;
+                $scope.guest.user.password = base64encode($scope.guest.user.email);
+                $scope.guest.user.userRole = {id: 5};
+                $scope.guest.user.regDate = (new Date()).toISOString().substr(0, 10);
 
-                    $scope.guest.numberReservations = 0;
-                    $scope.guest.status = 0;
+                $scope.guest.numberReservations = 0;
+                $scope.guest.status = 0;
 
-                    $scope.saveData("guest", $scope.guest, function (newGuest) {
-                        $scope.reservationGuest.guest = newGuest;
-                        $scope.saveData("reservation-guest", $scope.reservationGuest, function (newReservationGuest) {
-                            console.log(newReservationGuest);
+                $scope.saveData("guest", $scope.guest, function (newGuest) {
+                    $scope.reservationGuest.guest = newGuest;
+                    $scope.saveData("reservation-guest", $scope.reservationGuest, function (newReservationGuest) {
+                        console.log(newReservationGuest);
 
-                            if (isGroup)
-                                $scope.addReservationsToGroup(angular.copy(newReservation));
+                        if (isGroup)
+                            $scope.addReservationsToGroup(angular.copy(newReservation));
 
-                            $scope.reservationInfo = newReservation;
-                            $scope.reservationInfo.reservationGuests = [newReservationGuest];
+                        $scope.reservationInfo = newReservation;
+                        $scope.reservationInfo.reservationGuests = [newReservationGuest];
 
-                            $scope.clearEverything();
+                        $scope.clearEverything();
 
-                            $state.go('app.root.reservationSuccessful');
-                        });
-                    }, $scope.resetReservation);
-                });
+                        $state.go('app.root.reservationSuccessful');
+                    });
+                }, $scope.resetReservation);
             }
             else { // use existing guest
                 $scope.reservationGuest.guest = $scope.guest;
