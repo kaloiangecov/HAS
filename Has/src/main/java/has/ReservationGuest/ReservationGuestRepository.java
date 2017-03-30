46176e24d43650cb661b93e1ba44be1082c64350
Stefan <hardmaster.92@hotmail.com>
2017-03-30 14:09:09 +0300

    Fixed ReservationGuest query that returns multiple results in save Request.s

--- Has/src/main/java/has/ReservationGuest/ReservationGuestRepository.java
+++ Has/src/main/java/has/ReservationGuest/ReservationGuestRepository.java
@@ -3,11 +3,13 @@ package has.ReservationGuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by kaloi on 1/31/2017.
  */
 @Repository
 public interface ReservationGuestRepository extends JpaRepository<ReservationGuest, Long> {
 
-    ReservationGuest findByReservationStatusAndGuestId(Integer status, Long guestId);
+    List<ReservationGuest> findByReservationStatusAndGuestIdOrderByReservationStartDateDesc(Integer status, Long guestId);
 }
